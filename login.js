eedc74a77c75e811a99e4cabdcd1e50c327ff5cb
soul047 <minhoosoul77@gmail.com>
2025-08-16 18:15:48 +0900

    login js 추가

---
+++ login.js
@@ -0,0 +1,13 @@
+// login.js
+
+function loginWithEmail() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    // 여기에 실제 이메일/비밀번호 로그인 로직을 구현합니다.
+    alert('로그인 기능 구현 예정입니다.');
+}
+
+function socialLogin(provider) {
+    // 여기에 소셜 로그인 로직을 구현합니다.
+    alert(`${provider} 로그인 기능 구현 예정입니다.`);
+}
